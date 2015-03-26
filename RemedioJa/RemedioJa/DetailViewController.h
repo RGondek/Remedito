129a2a876ed3d7b106e06055852e8117f4d2ac0d
Rubens <rubens.gondek@gmail.com>
2015-03-26 14:47:48 -0300

    Layout da tabela aprimorado, célula Pronta

--- RemedioJa/RemedioJa/DetailViewController.h
+++ RemedioJa/RemedioJa/DetailViewController.h
@@ -11,7 +11,16 @@
 @interface DetailViewController : UIViewController
 
 @property (strong, nonatomic) id detailItem;
+
 @property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *imgRemedio;
+@property (weak, nonatomic) IBOutlet UILabel *nomeRemedio;
+@property (weak, nonatomic) IBOutlet UILabel *apRemedio;
+@property (weak, nonatomic) IBOutlet UILabel *compRemedio;
+@property (weak, nonatomic) IBOutlet UILabel *pDeRemedio;
+@property (weak, nonatomic) IBOutlet UILabel *pAteRemedio;
+
+@property (weak, nonatomic) IBOutlet UITableView *tableViewF;
 
 @end
 
